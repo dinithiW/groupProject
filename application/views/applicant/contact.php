4c1736c2f07d920dd8f5e5be729cd2f1dfd85dba
dinithiW <36946185+dinithiW@users.noreply.github.com>
2018-09-12 22:19:29 +0530

    added contact us page

---
+++ application/views/applicant/contact.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" href="<?=base_url('assets/css/contact.css')?>" type="text/css">
+</head>
+<body>
+	<div class="w3-main" style="margin-left:250px">
+			<div class="w3-row w3-padding-64">
+	<div id="first">
+		<div id="name">
+			<h1>contact</h1>
+		</div>
+	</div>
+	
+	<div class="w3-row w3-padding-64">
+    <div class="w3-twothird w3-container">
+	<h1 class="w3-text-teal"><b> Mailing Address </h1>
+	</div>
+	</div>
+	<div id="font1">
+		<p> University of Colombo School of Computing </p>
+		<p> UCSC Building Complex, </p>
+		<p> 35,Reid Avenue,Colombo 7</p>
+		<p>SRI LANKA </p>
+	</div>
+	<br>
+	<br>
+	<h1><b> Telephone </b></h1>
+	<div id="font1">
+		<p>+94 -11- 2581245/ 7</p>
+		</div>
+	
+	<br>
+	<br>
+	<h1><b> Fax </b></h1>
+	<div id="font1">
+		<p>+94-11-2587239</p>
+		</div>
+	<br>
+	<br>
+	<h1><b> Email </b></h1>
+	<div id="font1">
+		<p>info[at]ucsc.cmb.ac.lk</p>
+		</div>
+	</div>
+</div>
+
+</body>
+</html>
