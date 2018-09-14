36edb355d9cfad8a34d64f61ebde75cadc7647d4
dinithiW <36946185+dinithiW@users.noreply.github.com>
2018-09-14 21:07:43 +0530

    new controller to handle activities of the operator

---
+++ application/controllers/OperatorDashboard.php
@@ -0,0 +1,25 @@
+<?php
+
+class OperatorDashboard extends CI_Controller{
+
+	public function __construct() {
+		parent::__construct();
+		$this->load->helper('url');
+    }
+    
+    //completed
+	public function index(){
+        $this->load->view('users/operator/header');
+        $this->load->view('users/operator/createAd');
+        $this->load->view('users/operator/footer');
+	}
+
+
+    public function ad(){
+    	$this->load->view('users/operator/header');
+    	$this->load->view('users/operator/createAd');
+    	$this->load->view('users/operator/footer');
+    }
+
+}
+?>
