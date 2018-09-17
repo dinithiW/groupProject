2783f90692154f999bd2cd35a24a810a6ec9fde4
dinithiW <36946185+dinithiW@users.noreply.github.com>
2018-09-17 23:34:29 +0530

    corrected minor mistakes in prompt

--- application/views/messages/incorrectPassword.php
+++ application/views/messages/incorrectPassword.php
@@ -4,14 +4,9 @@
 <!-- <link rel="stylesheet" href="<?=base_url('assets/css/alerts/sweetalert.css')?>" type="text/css"/> -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.css">
   <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
-  
-
-
   <script src="https://code.jquery.com/jquery-2.1.3.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert-dev.js"></script>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.css">
-
-
 
 </head>
 <body>
@@ -19,14 +14,14 @@
 
 setTimeout(function () { 
 swal({
-  title: 'The password you entered is incorrect',
+  title: 'The passwords you entered do not match',
   text: 'Please check your password again',
   type: 'warning',
   confirmButtonText: 'OK'
 },
 function(isConfirm){
   if (isConfirm) {
-    window.location.href = 'index';
+    window.location.href = 'register';
   }
 }); }, 1);
 
