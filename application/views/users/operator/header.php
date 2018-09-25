1b8744674791d81fb051675ce2191756bdc82757
dinithiW <36946185+dinithiW@users.noreply.github.com>
2018-09-25 10:24:46 +0530

    changed colours of header

--- application/views/users/operator/header.php
+++ application/views/users/operator/header.php
@@ -4,11 +4,12 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-<link rel="stylesheet" href="https://www.w3schools.com/lib/w3-theme-black.css">
+<link rel="stylesheet" href="https://www.w3schools.com/lib/w3-theme-light-green.css">
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <style>
 html,body,h1,h2,h3,h4,h5,h6 {font-family: "Roboto", sans-serif;}
+
 .w3-sidebar {
   z-index: 3;
   width: 250px;
@@ -16,14 +17,20 @@ html,body,h1,h2,h3,h4,h5,h6 {font-family: "Roboto", sans-serif;}
   bottom: 0;
   height: inherit;
 }
+
+a:hover {
+    text-decoration: none;
+    color: #FF00FF;
+}
+
 </style>
 <body>
 
 <!-- Navbar -->
 <div class="w3-top">
   <div class="w3-bar w3-theme w3-top w3-left-align w3-large">
-    <a class="w3-bar-item w3-button w3-right w3-hide-large w3-hover-white w3-large w3-theme-l1" href="javascript:void(0)" onclick="w3_open()"><i class="fa fa-bars"></i></a>
-    <a href="" class="w3-bar-item w3-button w3-theme-l1">Home</a>
+    <a class="w3-bar-item w3-button w3-right w3-hide-large w3-hover-white w3-large w3-theme-dark" href="javascript:void(0)" onclick="w3_open()"><i class="fa fa-bars"></i></a>
+    <a href="" class="w3-bar-item w3-button w3-theme-l1 w3-hover-white">Home</a>
     <a href="<?= base_url('OperatorIndex/emails') ?>" class="w3-bar-item w3-button w3-hide-small w3-hover-white">Emails</a>
     <a href="#" class="w3-bar-item w3-button w3-hide-small w3-hover-white">Progress</a>
     <a href="#" class="w3-bar-item w3-button w3-hide-small w3-hover-white">Reports</a>
@@ -39,10 +46,10 @@ html,body,h1,h2,h3,h4,h5,h6 {font-family: "Roboto", sans-serif;}
     <i class="fa fa-remove"></i>
   </a>
   <h4 class="w3-bar-item"><b>Menu</b></h4>
-  <a class="w3-bar-item w3-button w3-hover-black" href="<?= base_url('OperatorIndex/createAd') ?>">Create Ad</a>
-  <a class="w3-bar-item w3-button w3-hover-black" href="#">Categorize Applications</a>
-  <a class="w3-bar-item w3-button w3-hover-black" href="#">Set Interview Date</a>
-  <a class="w3-bar-item w3-button w3-hover-black" href="#">Send Emails to Applicants</a>
+  <a class="w3-bar-item w3-button w3-hover-green" href="<?= base_url('OperatorIndex/createAd') ?>">Create Ad</a>
+  <a class="w3-bar-item w3-button w3-hover-green" href="#">Categorize Applications</a>
+  <a class="w3-bar-item w3-button w3-hover-green" href="#">Set Interview Date</a>
+  <a class="w3-bar-item w3-button w3-hover-green" href="#">Send Emails to Applicants</a>
 </nav>
 
 <!-- Overlay effect when opening sidebar on small screens -->
