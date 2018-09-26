8d06c874460f24896d3a92f1b8f6e0530875ac62
dinithiW <36946185+dinithiW@users.noreply.github.com>
2018-09-26 23:31:27 +0530

    minor modifications

--- application/views/users/director/header.php
+++ application/views/users/director/header.php
@@ -9,7 +9,7 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> 
   
 <style>
 html,body,h1,h2,h3,h4,h5,h6 {font-family: "Roboto", sans-serif;}
@@ -40,9 +40,9 @@ a:hover {
     <a href="#" class="w3-bar-item w3-button w3-hide-small w3-hover-white">Reports</a>
     <a href="#" class="w3-bar-item w3-button w3-hide-small w3-hover-white">Contacts</a>
 
-    <ul class="nav navbar-nav navbar-right w3-hover-white">
+    <ul class="nav navbar-nav navbar-right w3-hover-white" > 
       <li class="dropdown">
-       <a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="label label-pill label-danger count" style="border-radius:10px;"></span> <span class="glyphicon glyphicon-envelope" style="font-size:18px;"></span></a>
+       <a href="#" class="dropdown-toggle" data-toggle="dropdown" ><span class="label label-pill label-danger count" style="border-radius:10px; font-size:10px;"></span> <span class="glyphicon glyphicon-envelope" style="font-size:18px;"></span></a>
        <ul class="dropdown-menu"></ul>
       </li>
      </ul>
