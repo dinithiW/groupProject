31fccc1f49b541ac1d5e4b63304c4f3ddec6b005
dinithiW <36946185+dinithiW@users.noreply.github.com>
2018-09-27 22:55:12 +0530

    minor modifications

--- application/views/applicant/loginPanelApplicant.php
+++ application/views/applicant/loginPanelApplicant.php
@@ -14,6 +14,13 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+
+    <style>
+
+    body{
+        background: linear-gradient(to bottom right, #ffff66 0%, #cc00ff 100%);
+    }
+    </style>
 </head> 
 
 <body>
