456d271249d009febb101ae5a733e5b357593b42
ruwanliyanage123 <ruwanliyanagegalle1994@gmail.com>
2018-09-30 16:27:42 +0530

    add oath and agreement to fourth page

--- application/views/applicant/applicationForm/ApplicationFormPartFour.php
+++ application/views/applicant/applicationForm/ApplicationFormPartFour.php
@@ -14,10 +14,11 @@
 <body>
     <div class="container"><!-- start of the container-->
         <h3 class="panel-title" id="header1">  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b>APPLICANT REGISTRATION FORM  Basic Personal Details</b></h3>
-            <!-- start referee table-->        
-            <h4 id="topics">Referees </h4>
+            
+        <!-- start referee table-->        
+            <h4 id="title1">Referees </h4>
                     <div class="content3">
-                        <table id="Table">
+                        <table id="table1">
                             <tr class="tr">
                                 <td><input type="text" class="Text2" placeholder="        1" required size='5'/></td>
                                 <td><input type="text" class="Text1" placeholder="Name" required size='30'/></td>
@@ -49,7 +50,29 @@
                         </table>
                     </div>
     
-            <!-- end referee table-->    
+            <!-- end referee table-->   
+            
+                    <!-- notice-->   
+                    <h5 id="note1"><b> N.B.&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;When appling for the Academic Posts, one of the referees should be either the Professor or Senior Lecture of the Department of Study in<br> which the applicant had his/her University Education or the Head of the Institution in which the candidate works</b></h5>
+                    <!-- notice--> 
+
+                    <!-- other--> 
+                    <div form-group>
+                            <input type="text" name="other" id="other" class="form-control input-sm" placeholder="                                                                              Any other information that you need like to indicate ">
+                    </div> 
+                    <!-- other--> 
+
+                    <h4 id="oath">
+                        <b>I here by declare that the particulars furnished by me in the application are true and accurate. I am also aware that if any<br> partuculars contains herein are found to be false or incorrect I am liable to disqualification if the inaccuracy is discovered<br> before the selection and dimissal without any compensation if th inaccuracy is discovered after the appointment</b>
+                    </h4> 
+
+                     <div class="calender">
+                        <input id = "calender1"type="date" data-date-inline-picker="true" />
+                    </div>
+
+                    <div class="agreement">
+                        <input type="submit" value="I agree"  id ="gg"class ="btn btn-info btn-block acce">
+                    </div>
             
     </div><!-- end of the container-->
 </body>
