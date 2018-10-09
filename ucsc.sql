199559d180a77685a7ddff97ec62262473d5c37d
ruwanliyanage123 <ruwanliyanagegalle1994@gmail.com>
2018-10-09 21:08:49 +0530

    add higher_educational_details table for database

--- ucsc.sql
+++ ucsc.sql
@@ -265,3 +265,52 @@ COMMIT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+
+
+-- phpMyAdmin SQL Dump
+-- version 4.7.9
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 09, 2018 at 05:37 PM
+-- Server version: 10.1.31-MariaDB
+-- PHP Version: 7.2.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ucsc`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `higher_educational_details`
+--
+
+CREATE TABLE `higher_educational_details` (
+  `APPLICANT_ID` varchar(100) NOT NULL,
+  `UNIVERSITY` varchar(200) NOT NULL,
+  `FROM` varchar(10) NOT NULL,
+  `TO` varchar(10) NOT NULL,
+  `DEGREE_OBTAINED` varchar(200) NOT NULL,
+  `DURATION` varchar(10) NOT NULL,
+  `CLASS` varchar(10) NOT NULL,
+  `YEAR` varchar(10) NOT NULL,
+  `INDEX_NO` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
