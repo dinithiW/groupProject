0f23a8dcb99f2ddd7eb597fdcb6f2fe0a97c378e
ruwanliyanage123 <ruwanliyanagegalle1994@gmail.com>
2018-10-15 18:34:21 +0530

    more changes in form

--- application/views/applicant/applicationForm/TestForMultistepForms.php
+++ application/views/applicant/applicationForm/TestForMultistepForms.php
@@ -1,54 +1,53 @@
-<!DOCTYPE html>
 <html>
-<head>
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Test Page</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
-    <script src="main.js"></script>
-</head>
-<body>
-
-    <form id="msform">
-        <ul id="progressbar">
-            <li>account1</li>
-            <li>account2</li>
-            <li>account3</li>
-        </ul>
-        <fieldset>
-            <h2 class="fs-title">CREATE YOUR ACCOUNT</h2>
-            <h3 class="fs-subtitle">ADD DETAILS</h3>
-            <input type="text" name="email" placeholder="Email">
-            <input type="password" name="pass" placeholder="password">
-            <input type="password" name="cpass" placeholder="confirm password">
+    <head>
+        <title>thie for welocme</title>
         
-            <input type="button" name="next" value="Next">
-        </fieldset>
+        <link rel="stylesheet" href="<?=base_url('assets/css/applicationForm/TestForMultistepForms.css')?>" type="text/css"/>
+   </head>
 
-        <fieldset>
-            <h2 class="fs-title">PERSONAL DETAILS</h2>
-            <h3 class="fs-subtitle">ADD DETAILS</h3>
-            <input type="text" name="email" placeholder="Email">
-            <input type="password" name="pass" placeholder="password">
-            <input type="password" name="cpass" placeholder="confirm password">
-            <input type="button" name="previous" value="previous">
-            <input type="button" name="next" value="Next">
-        </fieldset>
+    <body>
+        <form id="msform">
+            <ul id="progressbar">
+                <li class="active">account1</li>
+                <li>account2</li>
+                <li>account3</li>
+            </ul>
 
-        <fieldset>
-            <h2 class="fs-title">SOCIAL DETAILS</h2>
-            <h3 class="fs-subtitle">ADD DETAILS</h3>
-            <input type="text" name="email" placeholder="Email">
-            <input type="password" name="pass" placeholder="password">
-            <input type="password" name="cpass" placeholder="confirm password">
-            <input type="button" name="previous" value="previous">
-            <input type="button" name="submit" value="submit">
-        </fieldset>
+            <fieldset>
 
-    </form>
+                <h2 class="fs-title">personal </h2>
+                <h2 class="fs-dubtitle">create your acconut</h2>
+                <input type="text" placeholder="email" name="email"/>
+                <input type="password" placeholder="password" name="pass"/>
+                <input type="password" placeholder="re enter password" name="cpass"/>
+                <input type="button" value="next" class="next action-button"/>
+
+            </fieldset>
+
+            <fieldset>
+
+                <h2 class="fs-title"></h2>
+                <h2 class="fs-dubtitle">educational</h2>
+                <input type="text" placeholder="email" name="email"/>
+                <input type="password" placeholder="password" name="pass"/>
+                <input type="password" placeholder="re enter password" name="cpass"/>
+                <input type="button" value="enter" class="next action-button"/>
+
+            </fieldset>
+
+            <fieldset>
+                <h2 class="fs-title"></h2>
+                <h2 class="fs-dubtitle">professional </h2>
+                <input type="text" placeholder="email" name="email"/>
+                <input type="password" placeholder="password" name="pass"/>
+                <input type="password" placeholder="re enter password" name="cpass"/>
+                
+                <input type="button" value="enter" class="next action-button"/>
+            </fieldset>
+
+        </form>
 
         <script src="http://thecodeplayer.com/uploads/js/jquery-1.9.1.min.js" type="text/javascript"></script>
         <script src="http://thecodeplayer.com/uploads/js/jquery.easing.min.js" type="text/javascript"></script>
-</body>
+    </body>
 </html>
