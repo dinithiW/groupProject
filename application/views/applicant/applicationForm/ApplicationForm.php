6342b6932656c367063c66ce2314f9786d4fa64c
ruwanliyanage123 <ruwanliyanagegalle1994@gmail.com>
2018-10-21 16:54:52 +0530

    add oath for form

--- application/views/applicant/applicationForm/ApplicationForm.php
+++ application/views/applicant/applicationForm/ApplicationForm.php
@@ -477,7 +477,29 @@
                         </table>
                     </div>
     
-            <!-- end referee table-->   
+            <!-- end referee table-->  
+
+                    <!-- notice-->   
+                    <h5 id="note1"><b> N.B.&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;When appling for the Academic Posts, one of the referees should be either the Professor or Senior Lecture of the Department of Study in<br> which the applicant had his/her University Education or the Head of the Institution in which the candidate works</b></h5>
+                    <!-- notice--> 
+
+                    <!-- other--> 
+                    <div form-group>
+                            <input type="text" name="other" id="other" class="form-control input-sm" placeholder="                                                                              Any other information that you need like to indicate ">
+                    </div> 
+                    <!-- other--> 
+
+                    <h4 id="oath">
+                        <b>I here by declare that the particulars furnished by me in the application are true and accurate. I am also aware that if any<br> partuculars contains herein are found to be false or incorrect I am liable to disqualification if the inaccuracy is discovered<br> before the selection and dimissal without any compensation if th inaccuracy is discovered after the appointment</b>
+                    </h4> 
+
+                     <div class="calender">
+                        <input id = "calender1"type="date" data-date-inline-picker="true" />
+                    </div>
+
+                    <div class="agreement">
+                        <input type="submit" value="I agree"  id ="Agree"class ="btn btn-info btn-block acce">
+                    </div> 
 
                     <input type="button" name="previous" class="previous button" value="Previous">
                     <input type="button" name="submit" class="next button" value="Finish">
