12c0d769c72a5d687927300260bef81ba39b902d
ruwanliyanage123 <ruwanliyanagegalle1994@gmail.com>
2018-10-29 14:09:45 +0530

    add function insertHigherEducationalDetailsModel

--- application/models/ApplicantApplicationFormModel.php
+++ application/models/ApplicantApplicationFormModel.php
@@ -129,7 +129,60 @@ class ApplicantApplicationFormModel extends CI_Model{
         $this->db->set($secondary_educational_table_fourth_row);
         $this->db->insert($this->db->dbprefix.'secondary_educational_details');
     
+    }
+
+
+    public function insertHigherEducationalDetailsModel(){
         
+        $this->load->database();
+        $secondary_educational_table_first_row = array(
+            'APPLICANT_ID'       =>"10",//////////////////////ad id number
+            'SCHOOL_NAME'        =>$this->input->post('secondary_educational_school_name1'),
+            'FROM'               =>$this->input->post('secondary_educational_from1'),
+            'TO'                 =>$this->input->post('secondary_educational_to1'),
+            'EXAMINATION_PASSED' =>$this->input->post('secondary_educational_examination1'),
+            'YEAR'               =>$this->input->post('secondary_educational_year1')
+        );
+
+        $this->db->set($secondary_educational_table_first_row);
+        $this->db->insert($this->db->dbprefix.'secondary_educational_details');
+        
+        $secondary_educational_table_second_row = array(
+            'APPLICANT_ID'       =>"10",//////////////////////ad id number
+            'SCHOOL_NAME'        =>$this->input->post('secondary_educational_school_name2'),
+            'FROM'               =>$this->input->post('secondary_educational_from2'),
+            'TO'                 =>$this->input->post('secondary_educational_to2'),
+            'EXAMINATION_PASSED' =>$this->input->post('secondary_educational_examination2'),
+            'YEAR'               =>$this->input->post('secondary_educational_year2')
+        );
+
+        $this->db->set($secondary_educational_table_second_row);
+        $this->db->insert($this->db->dbprefix.'secondary_educational_details');
+
+        $secondary_educational_table_third_row = array(
+            'APPLICANT_ID'       =>"10",//////////////////////ad id number
+            'SCHOOL_NAME'        =>$this->input->post('secondary_educational_school_name3'),
+            'FROM'               =>$this->input->post('secondary_educational_from3'),
+            'TO'                 =>$this->input->post('secondary_educational_to3'),
+            'EXAMINATION_PASSED' =>$this->input->post('secondary_educational_examination3'),
+            'YEAR'               =>$this->input->post('secondary_educational_year3')
+        );
+
+        $this->db->set($secondary_educational_table_third_row);
+        $this->db->insert($this->db->dbprefix.'secondary_educational_details');
+        
+        $secondary_educational_table_fourth_row = array(
+            'APPLICANT_ID'       =>"10",//////////////////////ad id number
+            'SCHOOL_NAME'        =>$this->input->post('secondary_educational_school_name4'),
+            'FROM'               =>$this->input->post('secondary_educational_from4'),
+            'TO'                 =>$this->input->post('secondary_educational_to4'),
+            'EXAMINATION_PASSED' =>$this->input->post('secondary_educational_examination4'),
+            'YEAR'               =>$this->input->post('secondary_educational_year4')
+        );
+
+        $this->db->set($secondary_educational_table_fourth_row);
+        $this->db->insert($this->db->dbprefix.'secondary_educational_details');
+    
     }
 
 
