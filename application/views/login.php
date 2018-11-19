a81d2dd0039ad1df255dbd722333bcab8d27e97b
dinithiW <36946185+dinithiW@users.noreply.github.com>
2018-11-19 23:24:54 +0530

    minor modifications

--- application/views/login.php
+++ application/views/login.php
@@ -53,7 +53,7 @@
             </div>
         <?php } ?>
         
-        <form action="<?php echo base_url(); ?>loginMe" method="post">
+        <form action="<?php echo base_url("loginUsers/process"); ?>" method="post">
           <div class="form-group has-feedback">
             <input type="email" class="form-control" placeholder="Email" name="email" required />
             <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
