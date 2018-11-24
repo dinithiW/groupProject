beb2b7e79d1222c36df8a1070175797c86d40f82
dinithiW <36946185+dinithiW@users.noreply.github.com>
2018-11-25 01:16:08 +0530

    changed the form rules to suit the add user module

--- assets/js/addUser.js
+++ assets/js/addUser.js
@@ -16,19 +16,25 @@ $(document).ready(function(){
 		
 		rules:{
 			fname :{ required : true },
-			email : { required : true, email : true, remote : { url : baseURL + "checkEmailExists", type :"post"} },
-			password : { required : true },
-			cpassword : {required : true, equalTo: "#password"},
+			lname :{ required : true },
+			designation :{required : true},
+			address : {required : true},
+			email : { required : true, email : true, remote : { url : baseURL + "OperatorIndex/checkEmailExists", type :"post"} },
+			/*password : { required : true },
+			cpassword : {required : true, equalTo: "#password"},*/
 			mobile : { required : true, digits : true },
-			role : { required : true, selected : true}
+			gender : { required : true, selected : true}
 		},
 		messages:{
 			fname :{ required : "This field is required" },
+			lname :{ required : "This field is required" },
+			designation : {required: "This field is required"},
+			address : {required : "This field is required"},
 			email : { required : "This field is required", email : "Please enter valid email address", remote : "Email already taken" },
-			password : { required : "This field is required" },
-			cpassword : {required : "This field is required", equalTo: "Please enter same password" },
+			/*password : { required : "This field is required" },
+			cpassword : {required : "This field is required", equalTo: "Please enter same password" },*/
 			mobile : { required : "This field is required", digits : "Please enter numbers only" },
-			role : { required : "This field is required", selected : "Please select atleast one option" }			
+			gender : { required : "This field is required", selected : "Please select atleast one option" }			
 		}
 	});
 });
