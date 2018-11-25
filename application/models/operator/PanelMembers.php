1b71e7615ac28ef764a5dc67d8ecac2731a1c9d3
dinithiW <36946185+dinithiW@users.noreply.github.com>
2018-11-25 19:26:36 +0530

    method to add panel memeber

--- application/models/operator/PanelMembers.php
+++ application/models/operator/PanelMembers.php
@@ -21,6 +21,16 @@ class PanelMembers extends CI_Model{
 		$this->panelID = $_POST[]
 	}*/
 
+    function checkEmailExists($username)
+    {
+        $this->load->database();
+        $this->db->select("USERNAME");
+        $this->db->from("users");
+        $this->db->where("USERNAME", $username); 
+        $query = $this->db->get();  
+        //$this->db->where("isDeleted", 0);
+        return $query->result();
+    }
 
 	public function getAllMembers(){
         $Members = [];
@@ -141,8 +151,130 @@ class PanelMembers extends CI_Model{
   		return $data;
 	}
 
+    function addNew()
+    {
+        
+       // $this->load->model('user_model');
+       // $data['roles'] = $this->user_model->getUserRoles();
+            
+       // $this->global['pageTitle'] = 'CodeInsect : Add New User';
+        //redirect('OperatorIndex/addMember');
+        $this->load->view("includes/header");
+        $this->load->view("users/operator/addMember");
+        $this->load->view("includes/footer");
+        
+    }
+
+    function addNewUser(){
+        
+        $this->load->library('form_validation');
+            
+        $this->form_validation->set_rules('fname','First Name','trim|required|max_length[255]');
+        $this->form_validation->set_rules('lname','Last Name','trim|required|max_length[255]');
+        $this->form_validation->set_rules('mobile','Mobile Number','required|min_length[10]');
+        $this->form_validation->set_rules('gender','Gender','trim|required');
+        $this->form_validation->set_rules('email','Email','trim|required|valid_email|max_length[255]');
+        $this->form_validation->set_rules('designation','Designation','trim|required|max_length[255]');
+        $this->form_validation->set_rules('address','Address','trim|required|max_length[255]');
+        
+            
+            if($this->form_validation->run() == FALSE)
+            {
+                $this->addNew();
+            }
+            else
+            {
+                $fname = $this->input->post('fname');
+                $lname = $this->input->post('lname');
+                $email = $this->input->post('email');
+                $gender = $_POST['gender'];
+                $designation = $this->input->post('designation');
+                $address = $this->input->post('address');
+                $mobile = $this->input->post('mobile');
+                
+                $namesub = substr($fname,0,3);
+                $nameid = strtolower($namesub);
+
+                $this->load->database();
+                $this->db->select("ID");
+                $this->db->from("interview_panel");
+                $this->db->order_by("ID", "desc");
+                $query = $this->db->get(); 
+                $rowcount = $query->num_rows();
+        
+                if($rowcount>0){
+                    $row = $query->row(); 
+                    $ans = $row->ID;
+                    $panelID = "$nameid"."$ans";
+                }else{
+                    $panelID = "$nameid"."1";
+                }
+                
+                
+
+                $userInfoPanel = array('PANEL_ID'=>$panelID,'FNAME'=>$fname,'LNAME'=>$lname,'EMAIL'=>$email,'GENDER'=>$gender ,'CONTACT_NUMBER'=>$mobile,'DESIGNATION'=>$designation,'ADDRESS'=>$address);
+
+                $name = $fname." ".$lname;
+                $usertype = "Panel";
+
+                $userInfoUsers = array('USERNAME'=>$email, 'PASSWORD'=>md5("abc"), 'USER_TYPE'=>$usertype, 'NAME'=> $name);
+                
+                //$this->load->model('user_model');
+                $result = $this->addUsers($userInfoUsers);
+                
+                if($result > 0)
+                {
+                    $result1 = $this->addPanelUsers($userInfoPanel);
+                    if($result1>0){
+                        $this->session->set_flashdata('success', 'New User created successfully');
+                    }else{
+                        $this->session->set_flashdata('error', 'User creation failed');
+                    }
+                    
+                }
+                else
+                {
+                    $this->session->set_flashdata('error', 'User creation failed');
+                }
+                
+                redirect('OperatorIndex/addMember');
+            }
+    }
 	//add member to interview panel
-	public function add(){
+    
+    public function addPanelUsers($userInfoPanel){
+
+        $this->db->trans_start();
+        $this->db->insert('interview_panel', $userInfoPanel);
+       // $this->db->insert('interview_panel', $userInfo);
+       $rows =  $this->db->affected_rows();
+        $this->db->trans_complete();
+       if($rows>0){
+            return $rows;
+        }else{
+            return FALSE;
+        }
+        //echo"$hayhay";
+        
+
+    }
+
+    public function addUsers($userInfoUsers){
+
+        echo"anybodyyyyyy?????";
+        $this->db->trans_start();
+        $this->db->insert('users', $userInfoUsers);
+       $rows =  $this->db->affected_rows();
+       $this->db->trans_complete();
+        $this->db->trans_complete();
+       if($rows>0){
+            return $rows;
+        }else{
+            return FALSE;
+        }
+
+    }
+	/*public function add(){
 
 		$fname = $this->testInput($_POST['fname']);
 		$lname = $this->testInput($_POST['lname']);
@@ -193,7 +325,7 @@ class PanelMembers extends CI_Model{
 
        redirect(base_url()."OperatorDashboard/memberSuccess");
        
-	}
+	}*/
 
 	
 
