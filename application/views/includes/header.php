99a2f45492e40774319e193e3abf29bba91a991e
dimuthi <32828462+dimuthi@users.noreply.github.com>
2019-01-02 22:57:45 +0530

    img

--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -102,13 +102,13 @@ if($checking=='1'){
               <!-- User Account: style can be found in dropdown.less -->
               <li class="dropdown user user-menu">
                 <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                  <img src="<?php echo base_url(); ?>assets/dist/img/avatar.png" class="user-image" alt="User Image"/>
+                  <img src="<?php echo base_url(); ?>assets/dist/img/girl.jpeg" class="user-image" alt="User Image"/>
                   <span class="hidden-xs"><?php echo $name; ?></span>
                 </a>
                 <ul class="dropdown-menu">
                   <!-- User image -->
                   <li class="user-header">
-                    <img src="<?php echo base_url(); ?>assets/dist/img/avatar.png" class="img-circle" alt="User Image" />
+                    <img src="<?php echo base_url(); ?>assets/dist/img/girl.jpeg" class="img-circle" alt="User Image" />
                     <p>
                       <?php echo $name; ?>
                       <small><?php echo $role_text; ?></small>
